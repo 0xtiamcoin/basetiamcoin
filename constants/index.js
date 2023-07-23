55df929980f92404609e411c8e9bf914456ac7d4
TiamCoin <122389954+tiamcoin-2021@users.noreply.github.com>
2023-07-23 16:52:47 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x2f6ac7990d0a2bb7c7c10e52cefa6bdb8773d3ae";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
